01a3e070ae919433d473b3cee39d102c85a2cf56
tracer-ysyx <tracer@ysyx.org>
2024-11-19 14:34:17 +0800

    > compile NEMU ysyx Yao Yibo Linux yyb-virtual-machine 6.8.0-48-generic #48~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Oct  7 11:24:13 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:34:17 up 39 min,  1 user,  load average: 0.09, 0.05, 0.06

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -55,6 +55,15 @@ static int cmd_q(char *args) {
 
 static int cmd_help(char *args);
 
+static int cmd_si(char *args) {
+	int step;
+	step = 0;
+	if(args == NULL) step = 1;
+	else sscanf(args,"%d",&step);
+	cpu_exec(step);
+	return 0;
+}
+
 static struct {
   const char *name;
   const char *description;
@@ -63,7 +72,7 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
-
+  {"si","Next Step",cmd_si },
   /* TODO: Add more commands */
 
 };

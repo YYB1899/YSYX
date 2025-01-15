f41799b647a4ee755953b2bae2059b9d9a111db9
tracer-ysyx <tracer@ysyx.org>
2025-01-15 16:12:19 +0800

    > compile NEMU ysyx Yao Yibo Linux yyb-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:12:19 up  6:34,  1 user,  load average: 0.23, 0.12, 0.10

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -234,8 +234,6 @@ uint32_t eval(int p, int q) {
          */
         return eval(p + 1, q - 1);
     }
-    else if(check_parentheses(p, q) == true) {
-    	assert(0);}
     else {
         int op = -1; // op = the position of 主运算符 in the token expression;
         bool simple = false;

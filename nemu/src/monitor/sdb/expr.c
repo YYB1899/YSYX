547304d96ceb8e8b52a0905a075dd5424211bcda
tracer-ysyx <tracer@ysyx.org>
2025-01-15 12:53:55 +0800

    > compile NEMU ysyx Yao Yibo Linux yyb-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:53:55 up  3:16,  1 user,  load average: 0.25, 0.18, 0.15

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -349,8 +349,8 @@ word_t expr(char *e, bool *success) {
   for(int i = 0 ; i < 5 ; i ++){
   	if(tokens[i].type == 11)
   	{
-  		printf("ac\n");
   		int hex_value = strtol(tokens[i].str,NULL,16);
+  		printf("hex=%d\n",hex_value);
   		int_to_char(hex_value,tokens[i].str);	
 	}
   }

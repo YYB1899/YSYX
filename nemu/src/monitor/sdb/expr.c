040af2146c94df465ffbf63a1a69a7ec95568c6f
tracer-ysyx <tracer@ysyx.org>
2025-01-15 12:20:22 +0800

    > compile NEMU ysyx Yao Yibo Linux yyb-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:20:22 up  2:50,  1 user,  load average: 0.11, 0.11, 0.09

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -220,9 +220,9 @@ int max(int a,int b){
 }
 
 uint32_t eval(int p, int q) {
-    if (p > q) {
     printf("p=%d\n",p);
     printf("q=%d\n",q);
+    if (p > q) {
         /* Bad expression */
         assert(0);
         return -1;

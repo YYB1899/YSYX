0b6f1a4303ba905e80f7dbf48652a82376becc81
tracer-ysyx <tracer@ysyx.org>
2025-01-15 15:44:29 +0800

    > compile NEMU ysyx Yao Yibo Linux yyb-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:44:29 up  6:06,  1 user,  load average: 0.04, 0.08, 0.08

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -422,7 +422,8 @@ word_t expr(char *e, bool *success) {
 	}
     }
         uint32_t result = 0;
- 	result = eval(0,nr_token - 1);
+        printf("%d,%d",tokens_len,nr_token);
+ 	result = eval(0,tokens_len - 1);
   	printf("result = %d\n", result);
   	memset(tokens,0,sizeof(tokens));
   	return result;  

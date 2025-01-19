b122920d82dd3db2c7835eda32ea9ddc67a62671
tracer-ysyx <tracer@ysyx.org>
2025-01-20 00:52:05 +0800

    > compile NEMU ysyx Yao Yibo Linux yyb-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:52:05 up  1:23,  1 user,  load average: 0.02, 0.02, 0.00

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -25,46 +25,6 @@ void engine_start();
 int is_exit_status_bad();
 
 int main(int argc, char *argv[]) {
-	printf("a");
-    FILE *input_fp = fopen("/home/yyb/ysyx-workbench/nemu/tools/gen-expr/input", "r");
-    if (input_fp == NULL) {
-        perror("Failed to open input file");
-        //return EXIT_FAILURE;
-    }
-    // 创建临时文件
-    FILE *temp_fp = fopen("/home/yyb/ysyx-workbench/nemu/tools/gen-expr/temp", "w");
-    if (temp_fp == NULL) {
-        perror("Failed to create temporary file");
-        fclose(input_fp);
-       // return EXIT_FAILURE;
-    }
-    char line[1024];  // 假设每行不超过1024个字符
-    while (fgets(line, sizeof(line), input_fp) != NULL) {
-        // 移除行尾的换行符（如果存在）
-        size_t len = strlen(line);
-        if (len > 0 && line[len-1] == '\n') {
-            line[len-1] = '\0';
-            len--;
-        }
-        // 获取要添加的内容
-        char *line_buf;
-        line_buf = strtok(line," ");
-        bool success = false;
-        int expr_res = expr(line_buf, &success);
-        // 写入原始行和附加内容到临时文件
-        fprintf(temp_fp, "%s = %d\n", line, expr_res);
-    }
-    // 关闭文件
-    fclose(input_fp);
-    fclose(temp_fp);
-   // 替换文件
-    if (rename("/home/yyb/ysyx-workbench/nemu/tools/gen-expr/temp", "/home/yyb/ysyx-workbench/nemu/tools/gen-expr/input") != 0) {
-        perror("Failed to rename temporary file to original file");
-        remove("/home/yyb/ysyx-workbench/nemu/tools/gen-expr/temp");  // 清理临时文件
-       // return EXIT_FAILURE;
-    }
-   printf("File processing completed successfully.\n");
-   // return EXIT_SUCCESS;
   /* Initialize the monitor. */
 #ifdef CONFIG_TARGET_AM
   am_init_monitor();
@@ -75,11 +35,6 @@ int main(int argc, char *argv[]) {
   /* Start engine. */
   engine_start();
   return is_exit_status_bad();
-  
-  
-//}
-
-//int main(int argc, char *argv[]) {
     
 }
 

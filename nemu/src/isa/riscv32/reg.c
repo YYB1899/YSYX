22b19c139852b0d5d5747f04bd1ea5bcebd8df0a
tracer-ysyx <tracer@ysyx.org>
2025-01-19 17:12:17 +0800

    > compile NEMU ysyx Yao Yibo Linux yyb-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:12:17 up 19:13,  1 user,  load average: 0.29, 0.15, 0.05

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -34,7 +34,7 @@ word_t isa_reg_str2val(const char *s, bool *success) {
   for(int i = 0 ; i < 32 ; i ++){
   	if(strcmp(regs[i],s) == 0){
   		return cpu.gpr[i];
-  		//*success = true;
+  		*success = true;
   	}
   } 
   return 0;

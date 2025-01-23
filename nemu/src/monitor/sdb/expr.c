5ecddfb6e2f1b58c5b1be33d48ad61b590b28dff
tracer-ysyx <tracer@ysyx.org>
2025-01-23 22:08:31 +0800

    > compile NEMU ysyx Yao Yibo Linux yyb-virtual-machine 6.8.0-51-generic #52~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Dec  9 15:00:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:08:31 up  2:15,  1 user,  load average: 0.07, 0.06, 0.01

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -213,6 +213,37 @@ uint32_t eval(int p, int q) {
 
 }
 }
+void int_to_char(int x, char str[]) {
+    memset(str, 0, 32); 
+    int tmp_index = 0;
+    int tmp_x = x;
+    int x_size = 0, flag = 1;
+    while (tmp_x) {
+        tmp_x /= 10;
+        x_size++;
+        flag *= 10;
+    }
+    flag /= 10;
+    while (x || flag) {
+        int a = x / flag;
+        x %= flag;
+        str[tmp_index++] = a + '0';
+        flag /= 10;
+    }
+    str[tmp_index] = '\0'; 
+}	
+
+int char_to_int(char s[]){
+    int s_size = strlen(s);
+    int res = 0 ;
+    for(int i = 0 ; i < s_size ; i ++)
+    {
+	res += s[i] - '0';
+	res *= 10;
+    }
+    res /= 10;
+    return res;
+}
 word_t expr(char *e, bool *success) {
     if (!make_token(e)) {
         *success = false;
@@ -249,7 +280,38 @@ word_t expr(char *e, bool *success) {
   	        
 	}
       }
-    
+        /*negative*/  
+  for(int i = 0 ; i < tokens_len ; i ++)
+    {
+	if(	(tokens[i].type == 3 && i > 0 
+		    && tokens[i-1].type != 1 && tokens[i-1].type != 11 && tokens[i-1].type != 12
+		    && tokens[i+1].type == 1 
+		    )
+                ||
+		(tokens[i].type == 3 && i > 0
+                    && tokens[i-1].type != 1 && tokens[i-1].type != 11 && tokens[i-1].type != 12
+                    && tokens[i+1].type == HEX
+                    )
+		||
+                (tokens[i].type == 3 && i == 0)
+          )	
+	{	
+	    tokens[i].type = 256;
+	    for(int j = 31 ; j >= 0 ; j --){
+		tokens[i+1].str[j] = tokens[i+1].str[j-1];
+	    }
+	    tokens[i+1].str[0] = '-' ;
+	    for(int j = 0 ; j < tokens_len ; j ++){
+	       if(tokens[j].type == 256)
+	       {
+		    for(int k = j +1 ; k < tokens_len ; k ++){
+			tokens[k - 1] = tokens[k];
+		    }
+		   tokens_len -- ;
+	       }
+	    }
+	}
+    }
     uint32_t result = eval(0, tokens_len - 1);
     printf("Result: %d\n", result);
     memset(tokens, 0, sizeof(tokens));

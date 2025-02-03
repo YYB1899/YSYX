3a7a46bb83aa5759bb7740d629653a3b7cbb907c
tracer-ysyx <tracer@ysyx.org>
2025-02-03 23:11:42 +0800

    > compile NEMU ysyx Yao Yibo Linux yyb-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:11:42 up  1:15,  1 user,  load average: 0.04, 0.04, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -188,7 +188,7 @@ int eval(int p, int q) {
         return -1;
     }
     else if (p == q) {
-        if(tokens[p].type == NUM || tokens[p].type == HEX || tokens[p].type == REG) {
+        if(tokens[p].type == NUM || tokens[p].type == HEX || tokens[p].type == TK_NOTYPE) {
             return atoi(tokens[p].str);
         }
         else{

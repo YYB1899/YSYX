9f85e9480573fe0da5f857bf31ba45bc21e18b8e
tracer-ysyx <tracer@ysyx.org>
2025-02-03 22:08:31 +0800

    > compile NEMU ysyx Yao Yibo Linux yyb-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:08:31 up 12 min,  1 user,  load average: 0.07, 0.19, 0.16

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -261,37 +261,6 @@ int eval(int p, int q) {
         }
     }
 }
-int char2int(char s[]){
-    int s_size = strlen(s);
-    int res = 0 ;
-    for(int i = 0 ; i < s_size ; i ++)
-    {
-	res += s[i] - '0';
-	res *= 10;
-    }
-    res /= 10;
-    return res;
-}
-void int2char(int x, char str[]){
-    int len = strlen(str);
-    memset(str, 0, len);
-    int tmp_index = 0;
-    int tmp_x = x;
-    int x_size = 0, flag = 1;
-    while(tmp_x){
-	tmp_x /= 10;
-	x_size ++;
-	flag *= 10;
-    }
-    flag /= 10;
-    while(x)
-    {
-	int a = x / flag; 
-	x %= flag;
-	flag /= 10;
-	str[tmp_index ++] = a + '0';
-    }
-}
 word_t expr(char *e, bool *success) {
     unsigned char m;
     unsigned char *n;
@@ -345,7 +314,6 @@ word_t expr(char *e, bool *success) {
              )||
              (tokens[i].type == SUB && i == 0)
            ){
-           //if(tokens[i].type == 3 && tokens[i - 1].type == 5 )	
 	    tokens[i].type = TK_NOTYPE;
 	    for(int j = 31 ; j >= 0 ; j --){
 		tokens[i+1].str[j] = tokens[i+1].str[j-1];

ad148f8c0b9be376a6c74b052e54967c929ed911
tracer-ysyx <tracer@ysyx.org>
2025-02-04 10:34:58 +0800

    > compile NEMU ysyx Yao Yibo Linux yyb-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:34:58 up  1:22,  1 user,  load average: 0.01, 0.04, 0.00

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -272,6 +272,14 @@ word_t expr(char *e, bool *success) {
         if (tokens[i].type == 0) break;
         tokens_len++;
     }
+        //HEX
+    for (int i = 0; i < tokens_len; i++) {
+        if (tokens[i].type == HEX) {
+            long int hex_value = strtol(tokens[i].str, NULL, 16);
+            snprintf(tokens[i].str, sizeof(tokens[i].str), "%ld", hex_value);
+            tokens[i].type = NUM;
+         }
+    }
     //REG//
     for(int i = 0 ; i < tokens_len ; i ++){
   	if(tokens[i].type == REG){
@@ -349,14 +357,7 @@ word_t expr(char *e, bool *success) {
 	    }
 	  }
     }
-    //HEX
-    //for (int i = 0; i < tokens_len; i++) {
-        //if (tokens[i].type == HEX) {
-            //long int hex_value = strtol(tokens[i].str, NULL, 16);
-            //snprintf(tokens[i].str, sizeof(tokens[i].str), "%ld", hex_value);
-            //tokens[i].type = NUM;
-       // }
-   // }
+
     uint32_t result = eval(0, tokens_len - 1);
     printf("result = %d\n", result);
     *success = true;

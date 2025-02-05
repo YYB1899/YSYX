dcaf0ac877054396ccff14628e65997ff40d010f
tracer-ysyx <tracer@ysyx.org>
2025-02-05 10:37:31 +0800

    > compile NEMU ysyx Yao Yibo Linux yyb-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:37:31 up  3:26,  1 user,  load average: 0.36, 0.22, 0.14

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -51,8 +51,8 @@ void init_mem() {
 }
 
 word_t paddr_read(paddr_t addr, int len) {
-  if (likely(in_pmem(addr))) return pmem_read(addr, len);
-  IFDEF(CONFIG_DEVICE) return mmio_read(addr, len);
+  if (likely(in_pmem(addr))) {printf("a\n");return pmem_read(addr, len);}
+  IFDEF(CONFIG_DEVICE) {printf("b\n");return mmio_read(addr, len);}
   out_of_bound(addr);
   return 0;
 }

0038eee313ff374404f57514f1671f2861206a4c
tracer-ysyx <tracer@ysyx.org>
2025-02-06 17:16:03 +0800

    > compile NEMU ysyx Yao Yibo Linux yyb-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:16:03 up 35 min,  5 users,  load average: 0.23, 0.22, 0.19

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -149,13 +149,12 @@ static int cmd_a(char *args){
         if (!success) {
             false_count++;
         }
+        }
         if (consecutive_false_count == total_count ) {
         printf("false_count=%d\n",false_count);
         printf("consecutive_false_count=%d\n",consecutive_false_count);
             assert(0);
         }
-        }
-
     fclose(input_fp);
     fclose(temp_fp);
 

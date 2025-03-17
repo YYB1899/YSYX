72ae2f0bbfe8d7a199f852f945918f4e1ce40349
tracer-ysyx <tracer@ysyx.org>
2025-03-17 18:59:29 +0800

    > compile NEMU ysyx Yao Yibo Linux yyb-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:59:29 up 17 min,  1 user,  load average: 0.26, 0.26, 0.25

--- npc/vsrc/imem.v
+++ npc/vsrc/imem.v
@@ -3,19 +3,11 @@ module imem (
     output reg [31:0] instruction // 32 位指令输出
 );
     reg [31:0] rom[255:0];
-    //initial begin 
-      //   $readmemb("file.txt",rom);
-    //end
-    
-    initial begin
-        rom[0] = 32'b000000000101_00010_000_00001_0010011; // addi x1, x2, 5
-        rom[1] = 32'b000000001010_00011_000_00010_0010011; // addi x2, x3, 10
-        rom[2] = 32'b000000001111_00100_000_00011_0010011; // addi x3, x4, 15
-        rom[3] = 32'b000000000001_00000_000_00000_1110011; // ebreak
+    initial begin 
+        $readmemb("/home/yyb/ysyx-workbench/npc/instruction.txt",rom);
         for (integer i = 4; i < 256; i = i + 1) begin
-            rom[i] = 32'b0; 
-        end
+           rom[i] = 32'b0; 
+        end   
     end
-
     assign instruction = rom[(pc - 32'h80000000)>>2];
 endmodule

525c5972875b02d45e5e44c6d272dcab9b21037d
tracer-ysyx <tracer@ysyx.org>
2025-03-26 20:27:27 +0800

    > compile NEMU ysyx Yao Yibo Linux yyb-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:27:27 up  1:32,  1 user,  load average: 0.01, 0.04, 0.05

--- npc/vsrc/imem.v
+++ npc/vsrc/imem.v
@@ -16,6 +16,10 @@ module imem (
         
         // 使用readmemh加载处理后的指令文件
         $readmemh("build/inst.hex", rom);
+        $display("===== Instruction Memory Initialization =====");
+        for (i = 0; i < 10; i = i + 1) begin
+            $display("imem[%h] = %h", 32'h80000000 + i*4, rom[i]);
+        end
      end   
   // 地址转换（按字寻址，支持80000000起始地址）
     wire [31:0] word_addr = (pc - 32'h80000000) >> 2;

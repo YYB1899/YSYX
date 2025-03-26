12c3d7b7a3927331ecd1b169ecaa88398d90def7
tracer-ysyx <tracer@ysyx.org>
2025-03-26 15:02:12 +0800

    > compile NEMU ysyx Yao Yibo Linux yyb-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:02:12 up  1:10,  1 user,  load average: 0.40, 0.13, 0.05

--- npc/vsrc/trap.v
+++ npc/vsrc/trap.v
@@ -17,15 +17,15 @@ module trap (
             if (instruction == 32'b000000000001_00000_000_00000_1110011) begin
                  $display("\n=================================");
             	$display(`GREEN,"  HIT GOOD TRAP (^_^)",`RESET);
-            	$display(`BOLD,"  PC: %h",`RESET, pc);
+            	$display("  PC: %h", pc);
             	$display("=================================\n");
                 $finish(0);
             end
             // BAD TRAP 检测（非法指令或溢出）
-            else begin
+            else if (overflow || (instruction[6:0] == 7'b1111111)) begin
                  $display("\n=================================");
             	$display(`RED,"  HIT BAD TRAP (X_X)",`RESET);
-            	$display(`BOLD,"  PC:        %h",`RESET, pc);
+            	$display("  PC:%h", pc);
             	$display("=================================\n");
                 $finish(1);
             end

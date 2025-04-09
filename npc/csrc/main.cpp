6d4a5ee85bdb069a1f73c725365dd79abefbca0f
tracer-ysyx <tracer@ysyx.org>
2025-04-09 17:05:28 +0800

    > compile NEMU ysyx Yao Yibo Linux yyb-virtual-machine 6.8.0-52-generic #53~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jan 15 19:18:46 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:05:28 up 11:14,  1 user,  load average: 0.50, 0.20, 0.13

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -34,6 +34,59 @@ void run_to_completion();
 void cmd_x(const std::string& args);
 void log_memory_access();
 
+#ifdef  DIFFTEST_ON
+
+extern uint64_t *dut_reg;
+extern uint64_t dut_pc;
+
+enum { DIFFTEST_TO_DUT, DIFFTEST_TO_REF };
+void (*ref_difftest_memcpy)(uint64_t addr, void *buf, size_t n, bool direction) = NULL;
+void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
+void (*ref_difftest_exec)(uint64_t n) = NULL;
+void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
+
+void difftest_init(char *ref_so_file, long img_size) {
+  assert(ref_so_file != NULL);
+
+  void *handle;
+  handle = dlopen(ref_so_file, RTLD_LAZY);
+  assert(handle);
+
+  ref_difftest_memcpy = (void (*)(uint64_t addr, void *buf, size_t n, bool direction))dlsym(handle , "difftest_memcpy");
+  assert(ref_difftest_memcpy);
+
+  ref_difftest_regcpy = (void (*)(void *dut, bool direction))dlsym(handle, "difftest_regcpy");
+  assert(ref_difftest_regcpy);
+
+  ref_difftest_exec = (void (*)(uint64_t n))dlsym(handle, "difftest_exec");
+  assert(ref_difftest_exec);
+
+  ref_difftest_raise_intr = (void (*)(uint64_t NO))dlsym(handle, "difftest_raise_intr");
+  assert(ref_difftest_raise_intr);
+
+  void (*ref_difftest_init)() = (void (*)())dlsym(handle, "difftest_init");
+  assert(ref_difftest_init);
+
+  ref_difftest_init();
+  ref_difftest_memcpy(PMEM_START,guest_to_host(PMEM_START), img_size, DIFFTEST_TO_REF);
+
+  regfile dut = pack_dut_regfile(dut_reg, INST_START);
+  ref_difftest_regcpy(&dut, DIFFTEST_TO_REF);
+}
+
+bool difftest_check() {
+  regfile ref,dut;
+  ref_difftest_regcpy(&ref, DIFFTEST_TO_DUT);
+  dut = pack_dut_regfile(dut_reg,dut_pc);
+  return checkregs(&ref, &dut);
+}
+
+void difftest_step() {
+  ref_difftest_exec(1);
+}
+
+#endif
+
 // 初始化 Capstone
 void init_capstone() {
     if (capstone_initialized) return;
@@ -196,6 +249,19 @@ int main(int argc, char** argv) {
         printf("  q    - Quit simulation\n");
 
         while (!contextp->gotFinish() && main_time < 100000) {
+            #ifdef DIFFTEST_ON
+    top->eval();                              // update rst_n_sync
+    if(top->i_clk && rst_n_sync){
+      //printf("pc = 0x%lx\n", dut_pc);
+      if(dut_pc != 0 && dut_pc != 1){         // 0 means branch bubble, 1 means data bubble.
+        if(!difftest_check()){                // check last cycle reg/mem.
+          print_regs();
+          break; 
+        }
+        difftest_step();                      // ref step and update regs/mem.
+      }
+    }
+#endif
             printf("(npc) ");
             fflush(stdout);
             

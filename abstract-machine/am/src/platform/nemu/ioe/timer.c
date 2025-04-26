464a3547b021e7d95b9f2b2a246c4b8eeabcb453
tracer-ysyx <tracer@ysyx.org>
2025-04-26 12:20:12 +0800

    > compile NEMU ysyx Yao Yibo Linux yyb-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  12:20:12 up  6:24,  1 user,  load average: 0.23, 0.19, 0.23

--- abstract-machine/am/src/platform/nemu/ioe/timer.c
+++ abstract-machine/am/src/platform/nemu/ioe/timer.c
@@ -13,9 +13,9 @@ void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
-  rtc->second = 00;
-  rtc->minute = 00;
-  rtc->hour   = 00;
+  rtc->second = 1;
+  rtc->minute = 0;
+  rtc->hour   = 0;
   rtc->day    = 0;
   rtc->month  = 0;
   rtc->year   = 1900;

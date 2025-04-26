8c88c13f1bfa68bc331757d9738a5f3bd90f1ca3
tracer-ysyx <tracer@ysyx.org>
2025-04-26 13:01:16 +0800

    > compile NEMU ysyx Yao Yibo Linux yyb-virtual-machine 6.8.0-57-generic #59~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Mar 19 17:07:41 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  13:01:16 up  7:05,  1 user,  load average: 0.15, 0.14, 0.11

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -17,77 +17,73 @@ int printf(const char *fmt, ...) {
 }
 
 int vsprintf(char *out, const char *fmt, va_list ap) {
-  //panic("Not implemented");
-  int len = 0;
-  while(*fmt != '\0'){
-  	if(*fmt != '%'){//当前字符不是 %
-  		*out = *fmt;
-  		out ++;
-  		fmt ++;
-  		len ++;
-  		continue;
-  	}//当前字符是 %
-  	fmt ++;//跳过 %，并检查下一个字符
-  	 int width = 0;
+    int len = 0;
+    while (*fmt != '\0') {
+        if (*fmt != '%') {
+            *out++ = *fmt++;
+            len++;
+            continue;
+        }
+        
+        fmt++;  // 跳过 %
+        int width = 0;
         int pad_zero = 0;
-  	// 解析修饰符（0 和宽度）
+
+        // 解析修饰符（0 和宽度）
         if (*fmt == '0') {
             pad_zero = 1;
             fmt++;
         }
-	while (*fmt >= '0' && *fmt <= '9') {
-    		width = width * 10 + (*fmt - '0');
-    		fmt++;
-	}
-  	switch(*fmt){
-  		case 's':{
-  			char* sp = va_arg(ap, char*);//获取一个指向字符串ap的指针sp
-  			while(*sp != '\0'){
-				*out = *sp;
-				out ++;
-				sp ++;
-				len ++;
-			}
-			break;
-  		}
-  		case 'd':{
-  			int num = va_arg(ap,int);  			
-  			char numb[12];
-  			int i = 0;
-  			if(num < 0){
-  				*out = '-';
-  				out ++;
-  				len ++;
-  				num = - num;
-  			}
-  			if(num == 0){
-  				*out = '0';
-  				out ++;
-  				len ++;
-  				break;
-  			}
-  			while(num != 0){//正向数字转换为字符串
-  				numb[i] = '0' + num % 10;//32:numb[0] = 2;numb[1] = 3 i = 2
-  				num /= 10;
-  				i ++;
-  			}
-  			while (i < width) {
-                    		numb[i++] = pad_zero ? '0' : ' ';
-                	}
-  			while(i > 0){//反向复制字符到输出缓冲区
-  				i --;
-				*out = numb[i];
-				out ++;
-				len ++;
-			}
-			break;
-  		}
-  	}
-  	fmt ++;
-  }
-  *out = '\0';//添加字符串结束符 \0
-  len ++;
-  return len;
+        while (*fmt >= '0' && *fmt <= '9') {
+            width = width * 10 + (*fmt - '0');
+            fmt++;
+        }
+
+        switch (*fmt) {
+            case 's': {
+                char *sp = va_arg(ap, char*);
+                while (*sp != '\0') {
+                    *out++ = *sp++;
+                    len++;
+                }
+                break;
+            }
+            case 'd': {
+                int num = va_arg(ap, int);
+                char buf[16];
+                int i = 0;
+
+                if (num < 0) {
+                    *out++ = '-';
+                    len++;
+                    num = -num;
+                }
+
+                do {
+                    buf[i++] = '0' + num % 10;
+                    num /= 10;
+                } while (num > 0);
+
+                while (i < width) {
+                    buf[i++] = pad_zero ? '0' : ' ';
+                }
+
+                while (i > 0) {
+                    *out++ = buf[--i];
+                    len++;
+                }
+                break;
+            }
+            default:
+                *out++ = '%';
+                *out++ = *fmt;
+                len += 2;
+                break;
+        }
+        fmt++;
+    }
+    *out = '\0';
+    return len;
 }
 int sprintf(char *out, const char *fmt, ...) { //不直接输出，是将结果写入 out 缓冲区
   //panic("Not implemented");

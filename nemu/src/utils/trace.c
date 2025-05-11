d6655c723ba0aa9d59c3d6d59256c6781940fb21
tracer-ysyx <tracer@ysyx.org>
2025-05-11 11:35:05 +0800

    > compile NEMU ysyx Yao Yibo Linux yyb-virtual-machine 6.8.0-58-generic #60~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Mar 28 16:09:21 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:35:05 up  1:41,  1 user,  load average: 0.42, 0.16, 0.24

--- nemu/src/utils/trace.c
+++ nemu/src/utils/trace.c
@@ -144,3 +144,16 @@ void display_ret_func(word_t pc){//显示函数返回的信息,pc:调用指令�
     for(int k = 0; k < rec_depth; k++) printf("  ");
     printf("ret  [%s]\n", symbol[i].name);
 }
+
+//DTRACE
+void display_device_read(paddr_t addr, int len, IOMap *map)
+{
+    log_write(ANSI_FMT("read memory: ", ANSI_FG_BLUE) FMT_PADDR ", the len is %d, the read device is " 
+                    ANSI_FMT(" %s ", ANSI_BG_BLUE) "\n", addr, len, map->name);
+}
+
+void display_device_write(paddr_t addr, int len, word_t data, IOMap *map)
+{
+    log_write(ANSI_FMT("write memory: ", ANSI_FG_YELLOW) FMT_PADDR ", the len is %d, the written data is " FMT_WORD 
+                    ", the written device is "ANSI_FMT(" %s ", ANSI_BG_YELLOW)"\n", addr, len, data, map->name);
+}

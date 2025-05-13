5b485dfeedf1dc88c7a7b6608dfefe595f8a58ed
tracer-ysyx <tracer@ysyx.org>
2025-05-13 15:19:47 +0800

    > compile NEMU ysyx Yao Yibo Linux yyb-virtual-machine 6.8.0-59-generic #61~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Tue Apr 15 17:03:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:19:47 up 17 min,  1 user,  load average: 0.47, 0.19, 0.17

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -4,12 +4,6 @@
 #define SYNC_ADDR (VGACTL_ADDR + 4)
 
 void __am_gpu_init() {
-  //int i;
-  //int w = 0;
-  //int h = 0;
-  //uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-  //for (i = 0; i < w * h; i ++) fb[i] = i;
-  //outl(SYNC_ADDR, 1);
 }
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
@@ -26,12 +20,12 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
   int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
   if (!ctl->sync && (w == 0 || h == 0)) return;
-  uint32_t *pixels = ctl->pixels;
-  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  uint32_t *pixels = ctl->pixels;//待绘制图像的像素数据数组
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;//帧缓冲区的指针
   uint32_t screen_w = inl(VGACTL_ADDR) >> 16;
   for (int i = y; i < y+h; i++) {
     for (int j = x; j < x+w; j++) {
-      fb[screen_w*i+j] = pixels[w*(i-y)+(j-x)];
+      fb[screen_w*i+j] = pixels[w*(i-y)+(j-x)]; //目标屏幕 = 源像素数据数组
     }
   }
   
